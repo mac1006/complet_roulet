7e3fbfad35210090ae33fa93e5e45a5973e937f2
mac1006 <67427704+mac1006@users.noreply.github.com>
2020-07-05 01:35:39 +0200

    Programm funktionsfähig in cmd

--- Farbroulet/src/Zahlenmehrspieler.py
+++ Farbroulet/src/Zahlenmehrspieler.py
@@ -7,7 +7,7 @@ Created on 25.06.2020
 import random
 
 asx = int(input('Anzahl der Spieler:\n'))
-a=0
+a = 0
 name = []
 satz = []
 farbe = []
@@ -15,53 +15,52 @@ guthaben = []
 
 for x in range(asx):
     a += 1
-    name.append(input('Name Spieler '+ str(a) + ':\n'))
+    name.append(input('Name Spieler ' + str(a) + ':\n'))
 for n in range(len(name)):
     guthaben.append(10)
 while True:
     for val1, val2 in zip(name, guthaben):
-            print(val1,' : ', val2, '\n')
+        print(val1, ' : ', val2, '\n')
     for n in range(0, len(name)):
+        satz.append(
+            input(name[n] + ", wie viel möchtest du setzen?\nDu hast " + str(guthaben[n]) + " Coins zur Verfügung\n"))
 
-        satz.append(input(name[n] + ", wie viel möchtest du setzen?\nDu hast " + str(guthaben[n]) + " Coins zur Verfügung\n"))
-        
     for val1, val2 in zip(name, satz):
-        print(val1,' : ', val2, '\n')
-        
+        print(val1, ' : ', val2, '\n')
+
     for n in range(0, len(name)):
         farbe.append(input(name[n] + ', wähle eine Zahl von 1 bis 36 oder eine Farbe\nr=rot\ns=schwarz\n'))
-        
+
     r = str(random.randint(0, 36))
     rotezahlen = ['1', '3', '5', '7', '9', '12', '14', '16', '18', '19', '21', '23', '25', '27', '30', '32', '34', '36']
-    schwarzezahlen = ['2', '4', '6', '8', '10', '11', '13', '15', '17', '20', '22', '24', '26', '28', '29', '31', '33', '35']
+    schwarzezahlen = ['2', '4', '6', '8', '10', '11', '13', '15', '17', '20', '22', '24', '26', '28', '29', '31', '33',
+                      '35']
 
     print(r)
     if r in rotezahlen:
         fa = 'r'
     else:
         fa = 's'
+    print(fa)
 
     for n in range(0, len(name)):
-        if farbe[n] in rotezahlen and r in rotezahlen:
-            print(name[n] + " hat " + str(int(satz[n]) * 35) + " Coins gewonnen\n")
-            guthaben[n] = guthaben[n] + (int(satz[n])*35)
-
-        elif farbe[n] in schwarzezahlen and r in schwarzezahlen:
+        if farbe[n] == r:
             print(name[n] + " hat " + str(int(satz[n]) * 35) + " Coins gewonnen\n")
             guthaben[n] = guthaben[n] + (int(satz[n]) * 35)
 
-        elif  farbe[n] == fa:
+        elif farbe[n] == fa:
             print(name[n] + " hat " + str(int(satz[n]) * 5) + " Coins gewonnen\n")
             guthaben[n] = guthaben[n] + (int(satz[n]) * 5)
+
         else:
             print(name[n] + " hat verloren\n")
             guthaben[n] = guthaben[n] - int(satz[n])
     farbe = []
     satz = []
 
-#elif farbe[n] in schwarzezahlen:
-#print(name[n] + " hat " + str(satz[n] * 5) + " Coins gewonnen\n")
-#guthaben[n] = guthaben[n] + (satz[n] * 5)
-#elif farbe[n] in rotezahlen:
-#print(name[n] + " hat " + str(satz[n] * 5) + " Coins gewonnen\n")
-#guthaben[n] = guthaben[n] + (satz[n] * 5)'''
+# elif farbe[n] in schwarzezahlen:
+# print(name[n] + " hat " + str(satz[n] * 5) + " Coins gewonnen\n")
+# guthaben[n] = guthaben[n] + (satz[n] * 5)
+# elif farbe[n] in rotezahlen:
+# print(name[n] + " hat " + str(satz[n] * 5) + " Coins gewonnen\n")
+# guthaben[n] = guthaben[n] + (satz[n] * 5)'''
